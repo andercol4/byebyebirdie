29a21f8cc2ffeed2bdad5ffb8cd28e82b69979a3
Colin Anderson <andercol.4@gmail.com>
2016-01-07 15:25:27 -0700

    react and es2015 social media knockoff

---
+++ app/assets/javascripts/components/Tweets.js.jsx
@@ -0,0 +1,53 @@
+class Tweets extends React.Component{
+  constructor(props){
+    super(props);
+    this.newTweet = this.newTweet.bind(this);
+    this.search = this.search.bind(this);
+    this.state = {tweets: []};
+  }
+  componentDidMount(){
+    $.ajax({
+      url: '/tweets',
+      type: 'GET'
+    }).success( data => {
+      this.setState({tweets: data.tweets});
+    });
+  }
+  search(){
+    $.ajax({
+      url: '/tweet_search',
+      type: 'GET',
+      data: {search_term: this.refs.search.value}
+    }).success( data => {
+      this.setState({tweets: data.tweets})
+    })
+  }
+  newTweet(){
+    $.ajax({
+      url:'/tweets',
+      type: 'POST',
+      data: {tweet: {body: this.refs.newTweet.value}}
+    }).success( data => {
+      let tweets = this.state.tweets;
+      tweets.unshift(data.tweet);
+      this.refs.newTweet.value = null;
+      this.setState({tweets: tweets});
+    });
+  }
+  render(){
+    let tweets = this.state.tweets.map( tweet => {
+      let key = `tweet-${tweet.id}`;
+      return (<Tweet key={key} {...tweet} />)
+    });
+    return(
+      <div className ='container'>
+        <input placeholder="What's on your mind?" ref='newTweet' autoFocus={true} />
+        <button className='btn' onClick={this.newTweet}>Post</button>
+        <input placeholder="Search" ref='search' onChange={this.search} />
+        <hr />
+        <h1 className='center'>Tweets</h1>
+        <hr />
+        {tweets}
+      </div>);
+  }
+}
